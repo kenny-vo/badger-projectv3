17b39df9d93f4aec54b533620023b472ba25aa97
Kenneth Vo <kennyvo91@gmail.com>
2017-08-01 14:31:23 -0500

    a

--- controllers/index.js
+++
@@ -1,2 +0,0 @@
-module.exports.api = require('./apiController');
-module.exports.listings = require('./listingsController');
--- models/index.js
+++
@@ -1,6 +0,0 @@
-var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/3000");
-
-var Listing = require('./listing');
-
-module.exports.Listing = Listing;
