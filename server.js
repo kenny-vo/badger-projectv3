b182a39a01e8c3c5a2a7abeac1c8cade21115c9b
Kenneth Vo <kennyvo91@gmail.com>
2017-08-02 08:59:52 -0500

    a

--- server.js
+++ server.js
@@ -33,6 +33,9 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
 
 // CONTACTS API ROUTES BELOW
 
+app.get('/api', controllers.api.index);
+
+
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
   console.log("ERROR: " + reason);
